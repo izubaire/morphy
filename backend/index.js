e4374cf01a3cc21f48a344ca0e0adab6c6f80f40
izubaire <m.zobaire@gmail.com>
2023-01-09 10:35:46 +0500

    google and twitter authentication

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,52 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import session from "express-session"
-import passport from "passport"
-import authRoute from "./routes/authRoute.js"
-import "./passport.js"
-import { errorHandler } from "./middlewares/errorMiddleware.js"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+import authRoute from "./routes/authRoute.js";
+import "./passport.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
-const ORIGIN = process.env.FRONTEND_ORIGIN
-const app = express()
+const PORT = process.env.PORT || 5000;
+const ORIGIN = process.env.FRONTEND_ORIGIN;
+const app = express();
 
 const sess = {
   secret: "some-secret",
   resave: true,
   saveUninitialized: true,
   cookie: {},
-}
+};
 
 if (process.env.NODE_ENV === "production") {
-  app.set("trust proxy", 1)
-  sess.cookie.secure = true
-  sess.cookie.sameSite = "none"
-  sess.cookie.maxAge = 1000 * 60 * 60 * 24 * 7 // One Week
+  app.set("trust proxy", 1);
+  sess.cookie.secure = true;
+  sess.cookie.sameSite = "none";
+  sess.cookie.maxAge = 1000 * 60 * 60 * 24 * 7; // One Week
 }
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
     origin: ORIGIN,
   })
-)
-app.use(session(sess))
-app.use(passport.initialize())
-app.use(passport.session()) //important because deserializeUser has to decode the information from the session id
+);
+app.use(session(sess));
+app.use(passport.initialize());
+app.use(passport.session()); //important because deserializeUser has to decode the information from the session id
 
-app.use("/auth", authRoute)
+app.use("/auth", authRoute);
 
 app.get("/", (req, res) => {
-  res.send("Hello from server")
-})
+  res.send("Hello from server");
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,36 +1,60 @@
-import express from "express"
-import passport from "passport"
+import express from "express";
+import passport from "passport";
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/login/failed", (req, res) => {
-  res.status(401)
-  throw new Error("Failed to authenticate")
-})
+  res.status(401);
+  throw new Error("Failed to authenticate");
+});
 
 router.get("/login/success", (req, res) => {
-  res.send(req.user)
-})
+  res.send(req.user);
+});
 
 router.get("/logout", (req, res, next) => {
   if (req.user) {
     req.logout(function (err) {
-      if (err) return next(err)
-      res.redirect(process.env.FRONTEND_ORIGIN)
-    })
+      if (err) return next(err);
+      res.redirect(process.env.FRONTEND_ORIGIN);
+    });
   }
-})
+});
 
 //github auth
-router.get("/github", passport.authenticate("github", { scope: ["user"] }))
+router.get("/github", passport.authenticate("github", { scope: ["user"] }));
 
 router.get(
   "/github/callback",
   passport.authenticate("github", { failureRedirect: "/auth/login/failed" }),
   function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect(process.env.FRONTEND_ORIGIN)
+    res.redirect(process.env.FRONTEND_ORIGIN);
   }
-)
+);
 
-export default router
+//google auth
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/auth/login/failed" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect(process.env.FRONTEND_ORIGIN);
+  }
+);
+
+//twitter auth
+router.get("/twitter", passport.authenticate("twitter"));
+
+router.get(
+  "/twitter/callback",
+  passport.authenticate("twitter", { failureRedirect: "/auth/login/failed" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect(process.env.FRONTEND_ORIGIN);
+  }
+);
+
+export default router;
