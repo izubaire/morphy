5cdb1fb98e92bf42bfb5d9c2908dbcde45c52abe
izubaire <m.zobaire@gmail.com>
2023-01-19 10:34:04 +0500

    registering user, firebase integration, adding loader and create image upload utility

---
+++ frontend/src/utils/firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app"
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const storage = getStorage(app)
+
+export { storage, ref, uploadBytesResumable, getDownloadURL }
