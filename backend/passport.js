eea407f537045b33d09f7e073420b99f7d391d16
izubaire <m.zobaire@gmail.com>
2023-01-20 17:18:42 +0500

    login with email and password

--- backend/passport.js
+++ backend/passport.js
@@ -1,14 +1,18 @@
 import gitHubStrategy from "passport-github2"
 import googleStrategy from "passport-google-oauth20"
 import twitterStrategy from "passport-twitter"
+import localStrategy from "passport-local"
 import passport from "passport"
 import dotenv from "dotenv"
+import UserModel from "./models/userModel.js"
+import bcrypt from "bcrypt"
 
 dotenv.config()
 
 const GitHub = gitHubStrategy.Strategy
 const Google = googleStrategy.Strategy
 const Twitter = twitterStrategy.Strategy
+const Local = localStrategy.Strategy
 
 passport.serializeUser(function (user, done) {
   done(null, user)
@@ -58,9 +62,15 @@ passport.use(
   )
 )
 
-//api-key: CgUW72NvzJ7QfZednNZVdep02
-//api-secret: yq0gY2dGo1QyTDKQSuKsnsg64twrqgFwTSlqdt25LJ4msKLXcR
-//bearer: AAAAAAAAAAAAAAAAAAAAAJ%2ByfQEAAAAAI6h2V2Xnjlp5vsGqLwgP%2F6fWbHM%3DSKXMpC77kszrrRHquUS510kTDjCinusqfzNtxsvTckZGSvpTe6
+passport.use(
+  new Local({ usernameField: "email" }, function (email, password, done) {
+    UserModel.findOne({ email }, function (err, user) {
+      if (err) return done(err)
+      if (!user || !bcrypt.compareSync(password, user.password))
+        return done(null, false)
+      return done(null, user)
+    })
+  })
+)
 
-//XeYqf65bAqMSzVpdJEMaiemoV
-//C4OXHM4BdIYjdoJFbzl5lgHErQbzMoNcukO01PDvE7Jy3mfLyZ
+
