852b357a4829d1178c72cc8d3542fafb6cc17365
izubaire <m.zobaire@gmail.com>
2023-03-04 15:30:09 +0500

    working author page and adding section on about page

--- backend/routes/articleRoute.js
+++ backend/routes/articleRoute.js
@@ -1,15 +1,14 @@
 import express from "express"
 import {
+  authorArticles,
   bookmarkArticle,
   createArticle,
   deleteArticle,
   getArticle,
   getArticles,
   getBookmarked,
-  mostBookmarked,
-  randomArticles,
-  recentArticles,
   updateArticle,
+  userArticles,
 } from "../controllers/articleController.js"
 import { verifyUser } from "../middlewares/authMiddleware.js"
 
@@ -21,9 +20,8 @@ router
   .put(verifyUser, updateArticle)
 
 router.get("/", getArticles)
-router.get("/random", randomArticles)
-router.get("/recent", recentArticles)
-router.get("/mostBookmarked", mostBookmarked)
+router.get("/author/:authorId", authorArticles)
+router.get("/:userId/user", userArticles)
 router.get("/:slug", getArticle)
 router.get("/:userId/usersBookmarked", verifyUser, getBookmarked)
 router.delete("/:userId/:articleId", verifyUser, deleteArticle)
