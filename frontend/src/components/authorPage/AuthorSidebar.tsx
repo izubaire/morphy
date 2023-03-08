b9ea655a3d1b405de3391f8de54fb79cf1064fd6
izubaire <m.zobaire@gmail.com>
2023-03-08 15:37:25 +0500

    add search page and fix bugs

--- frontend/src/components/authorPage/AuthorSidebar.tsx
+++ frontend/src/components/authorPage/AuthorSidebar.tsx
@@ -4,6 +4,7 @@ import AuthorsFeatured from "./AuthorsFeatured"
 import AuthorTags from "./AuthorTags"
 import Follow from "./Follow"
 import { useMemo } from "react"
+import { formateImg } from "../../utils/utilFunctions"
 
 interface Props {
   author: MongoUser
@@ -14,21 +15,23 @@ export default function AuthorSidebar({ author, articles }: Props) {
   const authorTags = useMemo(() => {
     const tags: string[] = []
     articles.forEach((a) => tags.push(...a.tags))
-    return tags
+    return Array.from(new Set(tags))
   }, [articles])
 
   return (
     <aside className="sticky top-32 left-0 h-fit">
       <AuthorImage
         authorName={author.displayName}
-        authorImage={author?.imgUrl ? author?.imgUrl : "/images/guest.jpg"}
+        authorImage={formateImg(author.imgUrl)}
       />
       <Follow />
-      <AuthorsFeatured
-        articles={articles.slice(-5)}
-        authorName={author.displayName}
-      />
-      <AuthorTags authorTags={authorTags} />
+      {articles.length ? (
+        <AuthorsFeatured
+          articles={articles.slice(-5)}
+          authorName={author.displayName}
+        />
+      ) : null}
+      {authorTags.length ? <AuthorTags authorTags={authorTags} /> : null}
     </aside>
   )
 }
